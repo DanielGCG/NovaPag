4fc564bf4eaf0503a21c1e7c99a3c7913dda45ea
DanielGCG <danielgcg@dcc.ufrj.br>
2024-10-31 19:16:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const PORT = process.env.PORT || 3000;
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota principal para servir o index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 // Rota dinâmica de exemplo
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Olá, esta é uma resposta dinâmica!' });
