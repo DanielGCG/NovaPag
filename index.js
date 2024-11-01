61e40552b7e215356341069f19c96d342954c701
DanielGCG <danielgcg@dcc.ufrj.br>
2024-10-31 21:12:19 -0300

    desisti da segurança

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config(); // Carregar variáveis de ambiente do .env
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Usar a variável de ambiente PORT ou 3000 como padrão
-
-// Configuração do Firebase
-const firebaseConfig = {
-    apiKey: process.env.FB_apiKey,
-    authDomain: process.env.FB_authDomain,
-    projectId: process.env.FB_projectId,
-    storageBucket: process.env.FB_storageBucket,
-    messagingSenderId: process.env.FB_messagingSenderId,
-    appId: process.env.FB_appId,
-};
-
-// Inicializando o Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-const appFirebase = initializeApp(firebaseConfig);
 
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
